753d46e462951879af936bbfe80a4194248f370b
Gaurang13 <delvadiya.gaurang2512@gmail.com>
2019-12-12 11:58:21 +0530

    program using code iterspection

---
+++ code_interspection.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+print(dir(Vehicle))
+print(id(Vehicle))
+print(type(Vehicle)) 
+print(callable(Vehicle))
