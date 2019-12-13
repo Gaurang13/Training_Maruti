bcd5096ca6e9abda6549f8746a1ebb88f3e43da4
Gaurang13 <delvadiya.gaurang2512@gmail.com>
2019-12-13 16:26:22 +0530

    create first program using flask and simple web page using flask and html

---
+++ Flask_Traning/app_index.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+
+app = Flask(__name__,template_folder="template")
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {"username": "gaurang"}
+    posts = [
+        {
+            'author': {"username": "Mahendra"},
+            'body': 'I think i have to give some time to my family'
+        },
+        {
+            'author': {"username": "Krupa"},
+            'body': 'Time is money'
+        }
+    ]
+    return render_template('index.html', user=user, title="name post", posts=posts)
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
