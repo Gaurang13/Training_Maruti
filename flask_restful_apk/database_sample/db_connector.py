0a34c8be5e179b048e30d7105dbc083a5d9e4219
Gaurang13 <46055406+Gaurang13@users.noreply.github.com>
2019-12-20 18:50:54 +0530

    Update db_connector.py

--- flask_restful_apk/database_sample/db_connector.py
+++ flask_restful_apk/database_sample/db_connector.py
@@ -4,9 +4,11 @@ from flask import current_app
 
 
 def connection_object():
-    return pymysql.connect(host=current_app.config['HOST'],
-                             user=current_app.config['USER'],
-                             password=current_app.config['PASSWORD'],
-                             db=current_app.config['DB'],
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+    from flask_restful_apk.main_pyfile import app
+    with app.app_context():
+        return pymysql.connect(host=current_app.config['HOST'],
+                                 user=current_app.config['USER'],
+                                 password=current_app.config['PASSWORD'],
+                                 db=current_app.config['DB'],
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
